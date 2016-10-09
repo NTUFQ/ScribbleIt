c95f0418276d2d052d74f7ef775885d5ca4d3d5c
Fu Qiang <FQ@FQWireless.local>
2016-10-09 12:29:41 +0800

    Initialized

---
+++ ScribbleIt/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ScribbleIt
+//
+//  Created by Fu Qiang on 9/30/16.
+//  Copyright © 2016 Fu Qiang. All rights reserved.
+//
+
+import UIKit
+import SwiftColorPickerViewController;
+
+class ViewController: UIViewController, SwiftColorPickerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
