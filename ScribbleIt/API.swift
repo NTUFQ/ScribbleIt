07b5cb4bb5a3401cc41a6445bb57fba9d6368d67
Fu Qiang <FQ@FQWireless.local>
2016-10-09 18:26:30 +0800

    Include Alamofire(HTTP Service) and swiftyJSON frameworks

---
+++ ScribbleIt/API.swift
@@ -0,0 +1,15 @@
+//
+//  API.swift
+//  ScribbleIt
+//
+//  Created by Fu Qiang on 10/9/16.
+//  Copyright © 2016 Fu Qiang. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class API{
+    
+}
